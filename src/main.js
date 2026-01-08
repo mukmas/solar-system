c4c4e8a1e1622019e929ca1cdc60d873877011f4
Lukas Hansen <mukmas00@gmail.com>
2026-01-08 12:54:50 +0100

    Add sphere and light

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three'
 import "./style.css"
-import { TextureSizeNode } from 'three/webgpu'
 
 const scene = new THREE.Scene()
 
@@ -11,12 +10,24 @@ const sizes = {
 
 const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height)
 scene.add(camera)
+camera.position.z = 20
 
 const canvas = document.querySelector('.webgl')
 const renderer = new THREE.WebGLRenderer({canvas})
 renderer.setPixelRatio(2)
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.03)
+scene.add(ambientLight)
+
+const geometry = new THREE.SphereGeometry(3, 64, 64)
+const material = new THREE.MeshStandardMaterial({
+    color: "#00ff83",
+    roughness: 0.5
+})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
 window.addEventListener("resize", () => {
     sizes.width = window.innerWidth
